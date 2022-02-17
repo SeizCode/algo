44bb095924416035e8a63ac3fe5f901970421ec7
k-vin <vinz.sosaity@gmail.com>
2022-02-17 13:59:49 +0100

    first commit

---
+++ exo.js
@@ -0,0 +1,20 @@
+var chiffre1=5;
+console.log(chiffre1);
+
+var chiffre2=4;
+var chiffre3=7;
+var resultat=chiffre2+chiffre3;
+console.log(resultat);
+var resultat1=chiffre2-chiffre3;
+console.log(resultat1);
+
+var chainecarac="bonjour tout le monde";
+console.log(chainecarac);
+
+var chainecarac1="coucou";
+var chainecarac2="petit perruche";
+
+
+
+var a=20;
+var b=30;
---
+++ index.py
@@ -0,0 +1,7 @@
+age = 30
+age
+30
+{
+  "php.validate.executablePath": "c:/php/php.exe"
+}
+Li
